8461c6a298c51a0c295cffe8cdf7ec2616f4e551
LeeJams <leejam208@gmail.com>
2021-12-11 21:30:40 +0900

    wss 웹소켓 공부 중

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,25 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on ws and http://localhost:3000");
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+// 없어도 ws 커넥션 연결은 됨.
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log(sockets);
+  console.log("Connected to Browser ⭕");
+
+  socket.on("close", () => console.log("Disconnected to Browser ❌"))
+
+  socket.on("message", (message) => {
+    sockets.forEach(each => each.send(message.toString()));
+    // socket.send(message.toString());
+  })
+})
+
+server.listen(3000, handleListen);
